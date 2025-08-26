fd1bec73d99636976a848fbb7346258b4746ec53
github-action-benchmark <github@users.noreply.github.com>
2025-08-26 00:09:36 +0000

    add n8n-mcp Benchmarks (customSmallerIsBetter) benchmark result for cac43ed3843e100c7a225a5519510c8f8d296595

---
+++ benchmarks/data.js
@@ -0,0 +1,60 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1756166976442,
+  "repoUrl": "https://github.com/Bobyyy70/n8n-mcp",
+  "entries": {
+    "n8n-mcp Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "56956555+czlonkowski@users.noreply.github.com",
+            "name": "Romuald Członkowski",
+            "username": "czlonkowski"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "cac43ed3843e100c7a225a5519510c8f8d296595",
+          "message": "Merge pull request #155 from czlonkowski/update-n8n-dependencies\n\nchore: update n8n dependencies to v1.107.4",
+          "timestamp": "2025-08-20T19:53:10+02:00",
+          "tree_id": "22242f23b6c77f2ac5d007164e37e81514fa536c",
+          "url": "https://github.com/Bobyyy70/n8n-mcp/commit/cac43ed3843e100c7a225a5519510c8f8d296595"
+        },
+        "date": 1756166975970,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sample - array sorting - small",
+            "value": 0.0187,
+            "unit": "ms",
+            "range": 0.3417,
+            "extra": "53335 ops/sec"
+          },
+          {
+            "name": "sample - array sorting - large",
+            "value": 3.1551,
+            "unit": "ms",
+            "range": 0.5334000000000003,
+            "extra": "317 ops/sec"
+          },
+          {
+            "name": "sample - string concatenation",
+            "value": 0.0047,
+            "unit": "ms",
+            "range": 0.2706,
+            "extra": "212894 ops/sec"
+          },
+          {
+            "name": "sample - object creation",
+            "value": 0.0659,
+            "unit": "ms",
+            "range": 0.3107,
+            "extra": "15177 ops/sec"
+          }
+        ]
+      }
+    ]
+  }
+}
